8d96a42335f01944e02fec69dfb2d3b90fa2c27c
Evan Xie <evanxie95@gmail.com>
2025-09-28 22:44:02 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,24 @@
+"""Module docstring"""
+
 MINIMUM_LENGTH = 10
 
-password = input("Password: ")
-while len(password) < MINIMUM_LENGTH:
-    print(f"Invalid password minimum length {MINIMUM_LENGTH} characters long")
+
+def main():
+    """Function docstring"""
+    password = get_password()
+    print_stars(password)
+
+
+def get_password():
     password = input("Password: ")
-print("*" * len(password))
+    while len(password) < MINIMUM_LENGTH:
+        print(f"Invalid password minimum length {MINIMUM_LENGTH} characters long")
+        password = input("Password: ")
+    return password
+
+
+def print_stars(password):
+    print("*" * len(password))
+
+
+main()
