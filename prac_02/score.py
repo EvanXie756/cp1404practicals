5ce97f9dc32365034f606809920ac2ddb6c980ef
Evan Xie <evanxie95@gmail.com>
2025-10-05 00:16:59 +1000

    determine score with random module

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,26 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+
+def main():
+    score = float(input("Enter score: "))
+    get_score(score)
+
+    random_score = random.randint(0,100)
+    print(f"Random Score: {random_score}")
+
+
+def get_score(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+main()
