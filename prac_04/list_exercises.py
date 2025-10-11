fc9cebfce4a7272b219891a0475dbaa953d2aceb
Evan Xie <evanxie95@gmail.com>
2025-10-12 01:05:39 +1000

    Checking lists with username

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,12 @@
+numbers = []
+
+for i in range(5):
+    number = int(input("Numbers: "))
+    numbers.append(number)
+
+print("The first number is ", numbers[0])
+print("The last number is ", numbers[-1])
+print("The smallest number is ", min(numbers))
+print("The largest number is ", max(numbers))
+print(f"The average of the number is", sum(numbers) / len(numbers))
+
