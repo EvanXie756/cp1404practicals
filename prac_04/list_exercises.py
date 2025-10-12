036d741235745c1ef862b6f06ef5cc518541b177
Evan Xie <evanxie95@gmail.com>
2025-10-12 23:44:40 +1000

    Basic list operations

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -10,3 +10,12 @@ print("The smallest number is ", min(numbers))
 print("The largest number is ", max(numbers))
 print(f"The average of the number is", sum(numbers) / len(numbers))
 
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
