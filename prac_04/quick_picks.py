eae71019e2cc078bfe3e77ac378bb38652b48dbe
Evan Xie <evanxie95@gmail.com>
2025-10-12 23:46:16 +1000

    Quick Pick Lottery Ticket Generator

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,1 +1,16 @@
+import random
+
+LOW_NUMBER = 1
+HIGHEST_NUMBER = 45
+NUMBERS_PER_PICK = 6
+
+number_of_picks = int(input("How many quick picks? "))
+for i in range(number_of_picks):
+    quick_picks = []
+    while len(quick_picks) < NUMBERS_PER_PICK:
+        number = random.randint(LOW_NUMBER, HIGHEST_NUMBER)
+        if number not in quick_picks:
+            quick_picks.append(number)
+    quick_picks.sort()
+    print(' '.join(f"{i:2}" for i in quick_picks))
 
