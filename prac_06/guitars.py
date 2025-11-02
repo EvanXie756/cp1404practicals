66edf50605fbc235bf35e2426046ee1ff2db2220
Evan Xie <evanxie95@gmail.com>
2025-11-02 23:55:23 +1000

    Using inputs in guitar file

---
+++ prac_06/guitars.py
@@ -0,0 +1,22 @@
+from guitar import Guitar
+
+
+def main():
+    print("My guitars!")
+    guitars = []
+
+    name = input("Name: ")
+    while name != "":
+        year = input("Year: ")
+        cost = float(input("Cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{name} ({year}) : {cost} added")
+        name = input("Name: ")
+
+    print("These are my guitars: ")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = "vintage" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+main()
