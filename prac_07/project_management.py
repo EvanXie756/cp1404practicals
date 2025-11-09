f25f982f6fbb2ffb38cf8cbb4aca06a3215921d3
Evan Xie <evanxie95@gmail.com>
2025-11-09 22:59:08 +1000

    adding menu

---
+++ prac_07/project_management.py
@@ -0,0 +1,20 @@
+from project import Project
+
+
+def main():
+    projects = ("project.txt")
+    print("Welcome to Pythonic Project Management")
+
+
+def menu():
+    print("- (L)oad projects")
+    print("- (S)ave projects")
+    print("- (D)isplay projects")
+    print("- (F)ilter projects by date")
+    print("- (A)dd new project")
+    print("- (U)pdate project")
+    print("- (Q)uit")
+
+
+
+main()
