3693e44a9da9c29256a5c56496785a6604474359
Evan Xie <evanxie95@gmail.com>
2025-11-16 21:46:45 +1000

    Add a list of names in dynamic app

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -2,3 +2,10 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
 
+
+class DynamicLabelsApp(App):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ["John", "Joe", "Jason", "Mary", "Kevin", "Juan"]
+
