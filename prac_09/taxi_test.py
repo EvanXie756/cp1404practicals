db812d0e862a8bcf7a46bcdc5ef8131cb828e8a0
Evan Xie <evanxie95@gmail.com>
2025-11-23 16:05:01 +1000

    Added distance driven

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -4,12 +4,9 @@ from prac_09.taxi import Taxi
 def main():
     my_taxi = Taxi("Prius 1", 100, 1.23)
 
-    my_taxi.drive(40)
+    distance_driven = my_taxi.drive(40)
+    my_taxi.current_fare_distance += distance_driven
 
-    print(my_taxi)
-    print("Current fare:", my_taxi.current_fare_distance)
 
-    my_taxi.current_fare_distance()
-    my_taxi.drive(100)
 
 main()
