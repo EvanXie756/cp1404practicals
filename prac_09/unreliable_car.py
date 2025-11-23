cdb57838072452f045740d812bc30e07b3b49271
Evan Xie <evanxie95@gmail.com>
2025-11-23 22:16:12 +1000

    Added random car reliability

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -4,13 +4,13 @@ import random
 
 class UnreliableCar(Car):
 
-    def __init__(self, name, fuel, reliability):
-        super.__init__(name, fuel)
-        self.reliability = reliability
+        def __init__(self, name, fuel, reliability):
+            super().__init__(name, fuel)
+            self.reliability = reliability
 
-    def drive(self, distance):
-        random_chance = random.random(0, 100)
-        if random_chance < self.reliability:
-            return super().drive(distance)
-        else:
-            return 0
+        def drive(self, distance):
+            random_chance = random.uniform(0, 100)
+            if random_chance < self.reliability:
+                return super().drive(distance)
+            else:
+                return 0
