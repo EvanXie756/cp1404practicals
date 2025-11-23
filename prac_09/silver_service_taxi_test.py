6c1911817a0a540982ee401bf36780602494b1fc
Evan Xie <evanxie95@gmail.com>
2025-11-23 23:07:30 +1000

    Added taxi other properties

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,6 +4,9 @@ from silver_service_taxi import SilverServiceTaxi
 def main():
     taxi = SilverServiceTaxi("Test taxi", 100, 2)
 
+    taxi.current_fare_distance()
+    taxi.drive(100)
+
 
 
 main()
