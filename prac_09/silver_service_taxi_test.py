55639b9c0c705f4991bb4406bac0d78522297a73
Evan Xie <evanxie95@gmail.com>
2025-11-23 23:21:51 +1000

    Added fare calculation

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,12 +4,13 @@ from silver_service_taxi import SilverServiceTaxi
 def main():
     taxi = SilverServiceTaxi("Test taxi", 100, 2)
 
-    taxi.current_fare_distance()
-    taxi.drive(100)
+    taxi.drive(18)
 
     fare = taxi.get_fare()
     print(f"Calculated fare: {fare:.2f}")
 
+    expected_fare = 48.78
+    assert fare == expected_fare, f"Fare should be {expected_fare}, got {fare}"
 
 
 main()
