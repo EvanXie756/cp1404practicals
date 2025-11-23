83b22c24dd873f156a147dc0b18de640206a6302
Evan Xie <evanxie95@gmail.com>
2025-11-23 16:42:40 +1000

    Removed the fare

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,7 +2,7 @@ from prac_09.taxi import Taxi
 
 
 def main():
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
 
     distance_driven = my_taxi.drive(40)
     my_taxi.current_fare_distance += distance_driven
