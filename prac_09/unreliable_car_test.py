a8998f0b1fa6b7165bf9786f1ccf252603bde229
Evan Xie <evanxie95@gmail.com>
2025-11-23 21:50:36 +1000

    Added reliability of the car

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,9 @@
+from unreliable_car import UnreliableCar
+
+
+def main():
+    reliable_car = UnreliableCar("Reliable", 100, 90)
+    unreliable_car = UnreliableCar("Unreliable", 100, 10)
+
+
+main()
