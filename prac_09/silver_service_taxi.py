ba1b0ae16723420b3b1bc6aede24eb094e9b583f
Evan Xie <evanxie95@gmail.com>
2025-11-23 22:59:23 +1000

    Added fare and flagfall together

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,16 @@
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+
+        self.price_per_km = Taxi.price_per_km * fanciness
+
+    def get_fare(self):
+        return super().get_fare() + self.flagfall
+
+
