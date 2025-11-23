bbc41c40fc0bfca7f4243328ed978d7d9d7f994f
Evan Xie <evanxie95@gmail.com>
2025-11-23 23:50:26 +1000

    Added menu functions

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -11,9 +11,38 @@ def main():
     current_taxi = None
 
     menu()
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "c":
+            print("Taxis available:")
+            for i, taxi in enumerate(taxis):
+                print(f"{i} - {taxi}")
+
+            try:
+                taxi_choice = int(input("Choose taxi: "))
+                current_taxi = taxi[taxi_choice]
+            except (ValueError, IndexError):
+                print("Invalid taxi choice")
+
+        elif choice == "d":
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
+
+            distance = float(input("Drive how far? "))
+            current_taxi.drive(distance)
+            trip_cost = current_taxi.get_fare()
+            bill_to_date += trip_cost
+
+        else:
+            print("Invalid option")
+
+        print(f"bill to date: ${bill_to_date:.2f}")
+        menu()
+        choice = input(">>> ").lower()
 
 
 def menu():
     print("q)uit, c)hoose taxi, d)rive")
+
 
 main()
