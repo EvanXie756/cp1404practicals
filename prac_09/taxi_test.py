4d08a549d5de0421ba2e8d3b7f10deab8d6acd7e
Evan Xie <evanxie95@gmail.com>
2025-11-23 16:07:13 +1000

    Printed taxi and fare

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,6 +7,10 @@ def main():
     distance_driven = my_taxi.drive(40)
     my_taxi.current_fare_distance += distance_driven
 
+    print(f"{my_taxi.name}, fuel={my_taxi.fuel}, odometer={my_taxi.odometer}")
+    print("Current fare:", my_taxi.current_fare_distance)
+
+
 
 
 main()
